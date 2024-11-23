f03ad3114cac9a5eaecd05b5b6832b9fe92a531a
javirrof <javirofe96@gmail.com>
2024-11-23 08:27:09 +0100

    fix issue with params in Pokemon page

--- src/app/pokemon/[pokemon]/page.tsx
+++ src/app/pokemon/[pokemon]/page.tsx
@@ -1,13 +1,11 @@
 import PokemonIndividual from '@/components/Pokemon/PokemonIndividual/PokemonIndividual'
 
-const PokemonIndividualPage = ({
-  params,
-}: {
-  params: {
-    pokemon: string
-  }
-}) => {
-  const pokemon = params.pokemon
+
+interface PageProps {
+  params: Promise<{ pokemon: string }>
+}
+const PokemonIndividualPage = async  ({params}: PageProps) => {
+  const pokemon = (await params).pokemon
   return (
     <div>
       <h1>Pokemon Individual Page {pokemon}</h1>
