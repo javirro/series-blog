9862463f301a59d50811069aca0535dbbc9a95f7
javirrof <javirofe96@gmail.com>
2024-11-23 08:14:26 +0100

    fix issue with props

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -1,9 +1,21 @@
 import PokemonsList from '@/components/Pokemon/PokemonList'
 import styles from '../app.module.css'
 import Link from 'next/link'
+import { Metadata } from 'next'
 
-const Page = async ({ searchParams }: { searchParams: { page: string } }) => {
-  const page = (await parseInt(searchParams.page)) || 1
+export const metadata: Metadata = {
+  title: 'Pokemon List',
+  description: 'A list of all the Pokemon from the first generation.',
+  keywords: ['pokemon', 'pokedex', 'generation 1'],
+}
+
+interface PageProps {
+  searchParams: Promise<{ page: string }>
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const searchP = await searchParams
+  const page = parseInt(searchP.page) || 1
   return (
     <section className={styles.page}>
       <h1>Pokemon List</h1>
@@ -19,6 +31,3 @@ const Page = async ({ searchParams }: { searchParams: { page: string } }) => {
 }
 
 export default Page
-
-
-
