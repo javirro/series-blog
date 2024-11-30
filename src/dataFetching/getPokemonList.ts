7edf40140c778d0295b7ade073dc69ce396ee1f2
javirrof <javirofe96@gmail.com>
2024-11-30 10:12:25 +0100

    add no cache

--- src/dataFetching/getPokemonList.ts
+++ src/dataFetching/getPokemonList.ts
@@ -3,7 +3,9 @@ import { URLS } from './urls'
 
 export const getPokemonList = async (offset: number, limit: number = 20) => {
   const pokemonListUrl = `${URLS['pokemon']}?limit=${limit}&offset=${offset}`
-  const pokemons = await fetch(pokemonListUrl)
+  const pokemons = await fetch(pokemonListUrl, {
+    cache: 'no-cache',
+  })
   const data: PokemonList = await pokemons.json()
   return data
 }
